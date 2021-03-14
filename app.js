bf77cb1e654bd7af5cf238751581cf1c41ef5352
John Kenny <squareindigojohn@yahoo.ie>
2021-03-14 17:49:16 +0000

    EJS functionality added to app

--- app.js
+++ app.js
@@ -3,25 +3,47 @@ const express = require('express');
 // sets up express app
 const app = express();
 
+// registers view engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    //res.send('<p>Home Page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    
+        const blogs = [
+            {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+            {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+            {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+          ];
+      
+
+    // renders an ejs view
+    res.render('index', { title: 'Home', blogs });
+
+
+    //res.sendFile('./views/index.html', { root: __dirname });
 });
 
 app.get('/about', (req, res) => {
-    //res.send('<p>About Page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', {title: 'About'});
+
+    //res.sendFile('./views/about.html', { root: __dirname });
 });
 
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create a new blog'});
+})
+
 // redirects to /about
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about'); 
+// });
 
 // 404 page, use acts like a default in a switch
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', {title: '404'});
+
+    //res.status(404).sendFile('./views/404.html', { root: __dirname });
 })
