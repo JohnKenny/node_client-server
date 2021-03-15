55ece5f0ce668351e09337b433650aaf47b82c36
John Kenny <squareindigojohn@yahoo.ie>
2021-03-15 16:01:55 +0000

    partial views added to app

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 
 
     //res.sendFile('./views/index.html', { root: __dirname });
-});
+}); 
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'});
