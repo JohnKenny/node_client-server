7ee4c5c15107bccf8f564f619f865ae72b7c0b4e
John Kenny <squareindigojohn@yahoo.ie>
2021-03-17 12:31:01 +0000

    mongo and mongoose functionality added

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { result } = require('lodash');
+const Blog = require('./models/blog');
+
+// connect to mongodb
+const dbURI = 'mongodb+srv://user34:Termina34@cluster0.xq4sd.mongodb.net/node-blogs?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true}) 
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
 // sets up express app
 const app = express();
 
@@ -7,13 +17,28 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// listen for requests
-app.listen(3000);
+
 
 // middleware and static files
 app.use(express.static('public'));
-
 app.use(morgan('dev'));
+
+//mongo and mongoose routing
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new',
+        snippet: 'about new',
+        body: 'more about'
+    });
+
+    blog.save()
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+})
 
 
 
